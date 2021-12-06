c5937e03c3750e7e001c31cb3855072c8da4b993
Supermel4 <melvincuperus02@gmail.com>
2021-12-06 13:50:03 +0100

    Als je al ingelogd was blijf je ingelogd

--- inloggen.php
+++ inloggen.php
@@ -3,6 +3,11 @@
 // Start session
 session_start();
 
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: activiteit.php");
+    exit;
+}
+
 ?>
 
 <!DOCTYPE html>
