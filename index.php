24aa71f61719560ab2808195f390a2559c3e0c38
Supermel4 <melvincuperus02@gmail.com>
2022-01-17 16:22:51 +0100

    htmlspecialchars toegevoegd

--- index.php
+++ index.php
@@ -90,14 +90,14 @@ $activiteiten->activiteitenOphalen();
                             
 
 							echo '
-							<div class="w-full h-68 flex md:justify-around justify-between flex-col mt-5 ml-5">
+							<div class="w-full h-68 flex md:justify-around justify-between flex-col mt-5 ml-2">
 							<div class="p-2 mx-2 my-2 place-items-center text-white bg-gray-400 rounded-3xl">
-							<h1 class="text-2xl text-center font-bold">'.ucfirst($item['activiteitnaam']).'</h1>
+							<h1 class="text-2xl text-center font-bold">'.htmlspecialchars(ucfirst($item['activiteitnaam'])).'</h1>
 							<br>
-							<h1 class="text-center">Op <b>'.$newStartDate.'</b> tot <b>'.$newEndDate.'</b></h1>
-							<h1 class="text-center">Locatie: <b>'.ucfirst($item['locatie']).'</b></h1>
+							<h1 class="text-center">Op <b>'.htmlspecialchars($newStartDate).'</b> tot <b>'.htmlspecialchars($newEndDate).'</b></h1>
+							<h1 class="text-center">Locatie: <b>'.htmlspecialchars(ucfirst($item['locatie'])).'</b></h1>
                             <br>
-                            <h1 class="text-center">Minimum aantal deelenemers: <b>'.($item['minimum']).'</b> en maximum aantal deelenemers <b>'.($item['maximum']).'</b></h1>
+                            <h1 class="text-center">Minimum aantal deelenemers: <b>'.htmlspecialchars(($item['minimum'])).'</b> en maximum aantal deelenemers <b>'.htmlspecialchars(($item['maximum'])).'</b></h1>
                             <h1 class="text-center"><b>'.$hoi.'</b> </h1>
                             <br>
                             '. ( ($plekkenOver <= 0) ? ("") : '<a href="aanmelden.php?id='.$item['id'].'""><button class="bg-green-400 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-2xl transition-colors duration-500"><i class="fas fa-plus"></i> Aanmelden</button></a>
